9346e5f25475631d1b288d477f8759ac46778e6c
Abraham Esrit Ines <esrit.ines@live.com.mx>
2017-02-27 16:36:20 -0600

    ajuste de gradiente

--- app/src/main/java/ines/esrit/dactilologico/MainActivity.java
+++ app/src/main/java/ines/esrit/dactilologico/MainActivity.java
@@ -1,8 +1,10 @@
 package ines.esrit.dactilologico;
 
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
